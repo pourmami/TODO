aa20b08514e361d22da3b8c37fe87db84e85a364
mohammad pourmami <m.pourmami@gmail.com>
2021-09-12 12:33:18 +0430

    set migrations

---
+++ database/migrations/2021_09_12_075509_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('text');
+
+            $table->foreign('post_id')->references('id')
+                ->on('posts')->onDelete('CASCADE');
+            $table->foreign('user_id')->references('id')
+                ->on('users')->onDelete('CASCADE');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
